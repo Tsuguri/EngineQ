42068bf34d442ce4d39dfe47fd65da9ac64e815f
Adam Winiarczuk <adamosca@gmail.com>
2016-07-17 15:34:31 +0200

    Finished camera movement

--- EngineQ/EngineQDemonstrationScripts/CameraMoveClass.cs
+++ EngineQ/EngineQDemonstrationScripts/CameraMoveClass.cs
@@ -10,12 +10,38 @@ namespace QScripts
 	{ 
 		private Vector3 tmp;
 		private Vector2 tmp2;
+		private float rotationX;
+		private float rotationY;
+		private float speed=5;
+		private bool reverseY=true;
+		private bool reverseX = true;
+		private static float pi = (float)Math.PI;
+
 		public CameraMoveClass()
 		{
 			Input.ListenKey(Input.Key.Escape, EscapeAction);
+			Input.ListenKey(Input.Key.F1, F1Action);
+			Input.ListenKey(Input.Key.F2, F2Action);
+			rotationX = transform.Rotation.EulerAngles.X;
+			rotationY = transform.Rotation.EulerAngles.Y;
 		}
 
+		private float DegToRad(float val)
+		{
+			return (float)(Math.PI*val/180);
+		}
 
+		private void CheckAngles()
+		{
+			if (rotationX > pi/2)
+				rotationX = pi/2;
+			if (rotationX < -pi/2)
+				rotationX = -pi/2;
+			if (rotationY > pi * 2)
+				rotationY -= pi * 2;
+			if (rotationY < 0)
+				rotationY += pi * 2;
+		}
 		protected override void Update()
 		{
 			base.Update();
@@ -36,7 +62,12 @@ namespace QScripts
 				MoveInDirection(tmp.Normalized);
 			if (Input.MouseButtonDown(1) && (tmp2=Input.MouseDeltaPosition).LengthSquared > 0)
 			{
-				
+				rotationX += DegToRad(tmp2.Y*(reverseX?1:-1))/speed;
+				rotationY += DegToRad(tmp2.X * (reverseY ? 1 : -1)) /speed;
+
+				CheckAngles();
+				Console.WriteLine($"Angles: {rotationX} {rotationY}");
+				transform.Rotation =Quaternion.CreateFromEuler(rotationX,rotationY,0);
 			}
 	}
 
@@ -49,6 +80,18 @@ namespace QScripts
 		{
 			if(action== Input.KeyAction.Press)
 				Application.Exit();
+		}
+
+		private void F1Action(Input.KeyAction action)
+		{
+			if (action == Input.KeyAction.Press)
+				reverseX = !reverseX;
+		}
+
+		private void F2Action(Input.KeyAction action)
+		{
+			if (action == Input.KeyAction.Press)
+				reverseY = !reverseY;
 		}
 	}
 }
