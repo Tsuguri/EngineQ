e35a801dcef5637e90f40cd27b9dfad658b941e1
Piotr Pełka <Piotr Peka>
2016-12-24 02:59:17 +0100

    More standard CMake

--- EngineQ/Scripts/GenMSVCSolution.bat
+++ EngineQ/Scripts/GenMSVCSolution.bat
@@ -1,4 +1,7 @@
+cd ..
+IF NOT EXIST Build MD Build
+cd Build
 IF NOT EXIST MSVC MD MSVC
 cd MSVC
 cmake -G "Visual Studio 14 Win64" ../../Source
-cd ..
+cd ../../Scripts
--- EngineQ/Scripts/GenMSVCSolution_clang.bat
+++ EngineQ/Scripts/GenMSVCSolution_clang.bat
@@ -1,4 +1,7 @@
+cd ..
+IF NOT EXIST Build MD Build
+cd Build
 IF NOT EXIST MSVC_clang MD MSVC_clang
 cd MSVC_clang
 cmake -G "Visual Studio 14 Win64" -T v140_clang_c2 ../../Source
-cd ..
+cd ../../Scripts
