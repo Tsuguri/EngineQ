d8e5c1c0e835aabc9511675a3d187e49404f0487
Adam Winiarczuk <adamosca@gmail.com>
2017-01-08 16:34:37 +0100

    Shapes

--- EngineQ/Source/EngineQScripting/Math/Shapes/Capsule.cs
+++ EngineQ/Source/EngineQScripting/Math/Shapes/Capsule.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace EngineQ.Math
 {
+	/// <summary>
+	/// Representation of capsule based on two points and capsule radius.
+	/// </summary>
 	public struct Capsule
 	{
+		/// <summary>
+		/// Coordinates of first <see cref="Capsule"/>'s point.
+		/// </summary>
 		public Vector3 Point1;
+
+		/// <summary>
+		/// Coordinates of second <see cref="Capsule"/>'s point.
+		/// </summary>
 		public Vector3 Point2;
+
+		/// <summary>
+		/// Radius of capsule.
+		/// </summary>
 		public float Radius;
 
 		public Capsule(Vector3 point1, Vector3 point2, float radius)
--- EngineQ/Source/EngineQScripting/Math/Shapes/Ray.cs
+++ EngineQ/Source/EngineQScripting/Math/Shapes/Ray.cs
@@ -1,8 +1,18 @@
 ﻿namespace EngineQ.Math
 {
+	/// <summary>
+	/// Math prepresentation of ray. It starts in Origin point and infinitely expands in given Direction.
+	/// </summary>
 	public struct Ray
 	{
+		/// <summary>
+		/// Start point of <see cref="Ray"/>.
+		/// </summary>
 		public Vector3 Origin;
+
+		/// <summary>
+		/// Direction in which <see cref="Ray"/> is expanding.
+		/// </summary>
 		public Vector3 Direction;
 
 		public Ray(Vector3 origin, Vector3 direction)
@@ -11,6 +21,11 @@
 			this.Direction = direction;
 		}
 
+		/// <summary>
+		/// Creates <see cref="Vector3"/> coordinates of point in given <paramref name="distance"/> from <see cref="Origin"/>.
+		/// </summary>
+		/// <param name="distance">Distance from <see cref="Origin"/> along <see cref="Direction"/> line.</param>
+		/// <returns>Coordinates of point.</returns>
 		public Vector3 GetPoint(float distance)
 		{
 			return Origin + Direction * distance;
